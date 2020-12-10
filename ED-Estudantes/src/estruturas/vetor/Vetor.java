b3d96705f5d6bb56269f42997ef00c69ca96e870
icarocrespo <icarocrespo@gmail.com>
2020-12-10 15:37:48 -0300

    Adição dos métodos de remoção e mostrar em Vetor

--- ED-Estudantes/src/estruturas/vetor/Vetor.java
+++ ED-Estudantes/src/estruturas/vetor/Vetor.java
@@ -7,10 +7,10 @@ import main.Mapa;
 
 /**
  *
- * @author Ketrin | Jéssica | Marina Classe Vetor
- * tempo_insercao -> Tempo para inserção de 100.000 estudantes
- * tempo_ordem -> Tempo para ordenamento crescente, por número de matrícula, dos estudantes
- * tempo_es -> Tempo para verificar o quantitativo de estudantes de Engenharia de Software
+ * @author Ketrin | Jéssica | Marina Classe Vetor tempo_insercao -> Tempo para
+ * inserção de 100.000 estudantes tempo_ordem -> Tempo para ordenamento
+ * crescente, por número de matrícula, dos estudantes tempo_es -> Tempo para
+ * verificar o quantitativo de estudantes de Engenharia de Software
  * tempo_remocao -> Tempo para remoção de estudantes com matrícula <= 20205000
  */
 public class Vetor {
@@ -21,7 +21,7 @@ public class Vetor {
     private long tempo_remocao;
     private Nodo[] vetor;
     private int tamanho;
-    
+
     public Vetor(int tamanho) {
         this.tamanho = tamanho;
         this.vetor = new Nodo[this.tamanho];
@@ -76,35 +76,76 @@ public class Vetor {
     }
 
 // Métodos aos requisitos
-    
     // Inserir 100.000 (cem mil) estudantes
-    public void inserir(Mapa mapa){
+    public void inserir(Mapa mapa) {
         long tempo = System.nanoTime();
         Iterator<Map.Entry<Integer, Estudante>> it = mapa.getEstudantes().entrySet().iterator();
-        while(it.hasNext()) {
+        while (it.hasNext()) {
             //this.inicio = it.iterator();
         }
         this.tempo_insercao = System.nanoTime() - tempo;
     }
-    
+
     // Apresentar todos os estudantes em ordem crescente de número de matricula
-    public void mostrarCrescente(){
+    public void mostrarCrescente() {
         long tempo = System.nanoTime();
-        
+        Nodo[] vetor_crescente = new Nodo[this.tamanho];
+
+        for (int i = 0; i < vetor.length; i++) {
+            for (int j = 0; j < vetor.length; j++) {
+                if (vetor[j].getMatricula() < vetor[i].getMatricula()) {
+                    vetor_crescente[i] = vetor[i];
+                }
+            }
+        }
+        mostrar(vetor_crescente);
         this.tempo_ordem = System.nanoTime() - tempo;
     }
-    
+
     // Verificar quantos estudantes realizam o curso Engenharia de Software
-    public void contarES(){
+    public int contarES() {
         long tempo = System.nanoTime();
-        
-        this.tempo_es = System.nanoTime() - tempo;
+        int count = 0;
+        for (int i = 0; i < this.tamanho; i++) {
+            if (this.vetor[i].getEstudante().getCurso().equals("Engenharia de Software")) {
+                count++;
+            }
+            this.tempo_es = System.nanoTime() - tempo;
+        }
+        return count;
     }
-    
-    // Remover todos os estudantes com número de matricula igual ou inferior a 202050000
-    public void remocao(){
+
+    // Remover todos os estudantes com número de matricula igual ou inferior a 202050000 (com novo vetor)
+    public Nodo[] remocaoNovo() {
         long tempo = System.nanoTime();
-        
+        Nodo[] vetor_removidos = new Nodo[this.vetor.length];
+        int posicao = 0;
+
+        for (int i = 0; i < vetor_removidos.length; i++) {
+            if (vetor[i].getMatricula() >= 202050000) {
+                vetor_removidos[posicao] = vetor[i];
+            }
+            posicao++;
+        }
         this.tempo_remocao = System.nanoTime() - tempo;
+
+        return vetor_removidos;
+    }
+
+    // Remover todos os estudantes com número de matricula igual ou inferior a 202050000 (no próprio vetor)
+    public void remocao() {
+        long tempo = System.nanoTime();
+
+        this.tempo_remocao = System.nanoTime() - tempo;
+    }
+
+    // Mostrar o vetor
+    public void mostrar(Nodo[] vetor) {
+        System.out.println("Mostrando registros da ED Vetor:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Posição " + i + ": Matrícula: "
+                    + vetor[i].getEstudante().getMatricula() + " | Curso: " + vetor[i].getEstudante().getCurso());
+        }
+        System.out.println("--- FIM ---");
     }
 }
